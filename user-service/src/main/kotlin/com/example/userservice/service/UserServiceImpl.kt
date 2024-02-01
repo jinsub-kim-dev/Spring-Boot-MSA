d19b9fe25008fd13fa2e3428f9098e1fc365f0c0
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2024-02-01 23:25:31 +0900

    feature:  User Service에 Spring Security 연동

--- user-service/src/main/kotlin/com/example/userservice/service/UserServiceImpl.kt
+++ user-service/src/main/kotlin/com/example/userservice/service/UserServiceImpl.kt
@@ -4,20 +4,22 @@ import com.example.userservice.dto.UserDto
 import com.example.userservice.jpa.UserEntity
 import com.example.userservice.jpa.UserRepository
 import org.modelmapper.ModelMapper
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class UserServiceImpl(
         private val userRepository: UserRepository,
-        private val modelMapper: ModelMapper
+        private val modelMapper: ModelMapper,
+        private val passwordEncoder: BCryptPasswordEncoder
 ) : UserService {
 
     override fun createUser(userDto: UserDto): UserDto {
         userDto.userId = UUID.randomUUID().toString()
 
         val userEntity = modelMapper.map(userDto, UserEntity::class.java)
-        userEntity.encryptedPwd = "encrypted_password"
+        userEntity.encryptedPwd = passwordEncoder.encode(userDto.pwd)
         this.userRepository.save(userEntity)
 
         return modelMapper.map(userEntity, UserDto::class.java)
